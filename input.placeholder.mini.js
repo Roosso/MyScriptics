5fa2ba525b6aecd83e3524b32387899f8d1b2426
Roosso <roosso@narod.ru>
2012-09-21 01:18:09 +0400

    велосипед

---
+++ input.placeholder.mini.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+    /* Placeholder for IE */
+    if($.browser.msie) { // Условие для вызова только в IE
+        $("form").find("input[type='text']").each(function() {
+            var tp = $(this).attr("placeholder");
+            $(this).attr('value',tp).css('color','#ccc');
+        }).focusin(function() {
+            var val = $(this).attr('placeholder');
+            if($(this).val() == val) {
+                $(this).attr('value','').css('color','#303030');
+            }
+        }).focusout(function() {
+            var val = $(this).attr('placeholder');
+            if($(this).val() == "") {
+                $(this).attr('value', val).css('color','#ccc');
+            }
+        });
+
+        /* Protected send form */
+        $("form").submit(function() {
+            $(this).find("input[type='text']").each(function() {
+                var val = $(this).attr('placeholder');
+                if($(this).val() == val) {
+                    $(this).attr('value','');
+                }
+            })
+        });
+    }
+});
